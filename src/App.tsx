a1760488b099a80125e546f9ff26fbd11dd3d7bf
Deivid-Martins <deividrocha2005@gmail.com>
2025-07-20 09:45:20 -0300

    feat: innitial header created

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Header } from "./components/Header/Header";
+
 function App() {
-  return <h1>Oi Mundo!</h1>;
+  return (
+    <>
+      <Header />
+      <h1>Oi Mundo!</h1>
+    </>
+  );
 }
 
 export default App;
