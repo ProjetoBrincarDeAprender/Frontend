d2f1eb191b9df2daf2bfb5e19daee531dbd85032
Deivid-Martins <deividrocha2005@gmail.com>
2025-07-21 10:16:32 -0300

    feat: created button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { BiHome } from "react-icons/bi";
 import { Header } from "./components/Header/Header";
+import { Button } from "./components/utils/Button/Button";
 
 function App() {
   return (
     <>
       <Header username="Deivid" />
+      <Button variant="primary" size="lg">
+        <BiHome /> Primary
+      </Button>
+      <Button variant="warning" size="lg">
+        <BiHome /> Warning
+      </Button>
+      <Button variant="light" size="lg">
+        <BiHome /> Light
+      </Button>
+      <Button variant="dark" size="lg">
+        <BiHome /> Dark
+      </Button>
     </>
   );
 }
