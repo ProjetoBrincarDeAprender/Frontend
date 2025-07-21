70e55495c4f1d06b76a0064ea73d3d6f42d698dc
Deivid-Martins <deividrocha2005@gmail.com>
2025-07-21 13:40:22 -0300

    feat: created button component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { BiHome } from "react-icons/bi";
 import { Header } from "./components/Header/Header";
 import { Button } from "./components/utils/Button/Button";
 import { Link } from "./components/utils/Link/Link";
-
+import { Input } from "./components/utils/Input/Input";
 function App() {
   return (
     <>
       <Header username="Deivid" />
+      <h1 className="text-red-600">--- Button Components Below ---</h1>
       <Button variant="primary" size="lg">
         <BiHome /> Primary
       </Button>
@@ -19,6 +20,7 @@ function App() {
       <Button variant="dark" size="lg">
         <BiHome /> Dark
       </Button>
+      <h1 className="text-red-600">--- Link Components Below ---</h1>
       <Link href="/" variant="primary">
         Teste
       </Link>
@@ -28,6 +30,25 @@ function App() {
       <Link href="/" variant="dark">
         Teste
       </Link>
+      <h1 className="text-red-600">--- Input Components Below ---</h1>
+      <Input
+        type="email"
+        inputSize="sm"
+        variant="primary"
+        label="Seu Email Aqui"
+      />
+      <Input
+        type="email"
+        inputSize="md"
+        variant="light"
+        label="Seu Email Aqui"
+      />
+      <Input
+        type="email"
+        inputSize="lg"
+        variant="dark"
+        label="Seu Email Aqui"
+      />
     </>
   );
 }
