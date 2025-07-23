7d07f5f424c846738962a28750d05c333b1b0aaa
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-07-23 20:50:58 -0300

    feat: adding dashboard route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router";
 import Form from "./pages/Form/Form";
+import Dashboard from "./pages/Dashboard/Dashboard";
 import { Home } from "./pages/Home/Home";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Routes>
       <Route index element={<Home />} />
       <Route path="/cadastrar" element={<Form />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,13 @@
+import { Footer } from "../../components/Footer/Footer";
+import { Header } from "../../components/Header/Header";
+import { LateralMenu } from "../../components/LateralMenu/LateralMenu";
+
+export default function dashboard() {
+  return (
+    <>
+      <Header />
+      <main className="mt-28">{<LateralMenu />}</main>
+      <Footer />
+    </>
+  );
+}
