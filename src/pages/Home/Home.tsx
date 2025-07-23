f3e939d3959eca58d91c7a050404b0295b0cf6df
Deivid-Martins <deividrocha2005@gmail.com>
2025-07-23 08:22:36 -0300

    feat: created PopularGames private component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from "../../components/Header/Header";
-import { Card } from "../../components/utils/Card/Card";
 import { Introduction } from "./components/Introduction/Introduction";
+import { PopularGames } from "./components/PopularGames/PopularGames";
 
 import "./Home.css";
 
@@ -11,7 +11,7 @@ export function Home() {
       <Header />
       <main className="flex flex-col m-auto py-16">
         <Introduction />
-        <Card gameIdUrl="/12" title="Jogo da Velha" />
+        <PopularGames className="mt-48" />
       </main>
       <Footer />
     </>
