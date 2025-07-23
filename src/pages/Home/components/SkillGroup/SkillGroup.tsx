76799168543f1f358e5f9295294f0a163283ae05
Deivid-Martins <deividrocha2005@gmail.com>
2025-07-23 08:46:22 -0300

    chore: fixed header on top and changed colors on skillgroup

--- src/pages/Home/components/SkillGroup/SkillGroup.tsx
+++ src/pages/Home/components/SkillGroup/SkillGroup.tsx
@@ -7,16 +7,16 @@ interface SkillGroupProps {
 
 export function SkillGroup({ className = "" }: SkillGroupProps) {
   return (
-    <section className={`${className} bg-red-50 py-32`}>
+    <section className={`${className} bg-blue-50 py-32`}>
       <div className="text-center">
         <h2 className="title-skillgroup font-1 font-bold text-4xl text-gray-800">
           HABILIDADES DESENVOLVIDAS
         </h2>
       </div>
       <div className="flex px-8 py-16">
-        <Card gameIdUrl="4" title="Jogo da Velha" variant="skill" />
-        <Card gameIdUrl="5" title="Sudoku" variant="skill" />
-        <Card gameIdUrl="6" title="Tetris" variant="skill" />
+        <Card gameIdUrl="4" title="Mamemática" variant="skill" />
+        <Card gameIdUrl="5" title="Português" variant="skill" />
+        <Card gameIdUrl="6" title="Geografia" variant="skill" />
       </div>
     </section>
   );
