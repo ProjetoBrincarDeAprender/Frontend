b39cfbdcbde23234dfda47faf80f3f3d74bb348c
Deivid-Martins <deividrocha2005@gmail.com>
2025-07-23 06:55:21 -0300

    feat: added react router and created home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { Footer } from "../../components/Footer/Footer";
+import { Header } from "../../components/Header/Header";
+
+export function Home() {
+  return (
+    <>
+      <Header />
+      Home Page
+      <Footer />
+    </>
+  );
+}
