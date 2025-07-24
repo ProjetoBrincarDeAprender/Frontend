3d30ff6f8ede68a442b2663ff19fbfcbcb3ff7df
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-07-24 20:06:54 -0300

    feat: made the main structure

--- src/components/LateralMenu/LateralMenu.tsx
+++ src/components/LateralMenu/LateralMenu.tsx
@@ -4,8 +4,25 @@ import profile from "../../assets/astronauta-profile.svg";
 export function LateralMenu() {
   return (
     <div className={styles.menuContainer}>
-      <img src={profile} alt="" />
-      <h1>Teste</h1>
+      <div className={styles.user}>
+        <img src={profile} alt="" />
+        <span className={styles.username}>Usuario Usuario</span>
+      </div>
+
+      <ul className={styles.itens}>
+        <li>
+          <a href="">Logística</a>
+        </li>
+        <li>
+          <a href="">Alunos</a>
+        </li>
+        <li>
+          <a href="">Professores</a>
+        </li>
+        <li>
+          <a href="">Escolas</a>
+        </li>
+      </ul>
     </div>
   );
 }
