9d4e76e8d15f47529741a23d47b71e05d3d30b45
Deivid-Martins <deividrocha2005@gmail.com>
2025-07-25 09:19:59 -0300

    feat: added route that redirect user to index, if route informed is not created

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Form from "./pages/Form/Form";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import { Home } from "./pages/Home/Home";
@@ -9,6 +9,7 @@ function App() {
       <Route index element={<Home />} />
       <Route path="/cadastrar" element={<Form />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
