ae23d7b62ef0c900a02cc3bfe55302e372600930
Deivid-Martins <deividrocha2005@gmail.com>
2025-07-29 07:43:09 -0300

    feat: created close and open functionality into LateralMenu

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,13 +2,13 @@ import { Footer } from "../../components/Footer/Footer";
 import { Header } from "../../components/Header/Header";
 import { LateralMenu } from "../../components/LateralMenu/LateralMenu";
 
+import "./Dashboard.css";
+
 export default function dashboard() {
   return (
     <>
       <Header />
-      <main className="mt-28 bg-slate-100">
-        {<LateralMenu />}
-      </main>
+      <main className="mt-28 bg-slate-100">{<LateralMenu />}</main>
       <Footer />
     </>
   );
