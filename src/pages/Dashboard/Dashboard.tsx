fe584aa21c4baf1fa5b8658be35610f07cf37a2e
Deivid-Martins <deividrocha2005@gmail.com>
2025-07-29 08:15:11 -0300

    feat: created InfoBadge component

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from "../../components/Header/Header";
 import { LateralMenu } from "../../components/LateralMenu/LateralMenu";
+import { InfoBadge } from "../../components/utils/InfoBadge/InfoBadge";
 
 import "./Dashboard.css";
 
@@ -8,7 +9,10 @@ export default function dashboard() {
   return (
     <>
       <Header />
-      <main className="mt-28 bg-slate-100">{<LateralMenu />}</main>
+      <main className="mt-28 bg-slate-100">
+        <LateralMenu />
+        <InfoBadge variant="blue" label="Professores Ativos" value="12" />
+      </main>
       <Footer />
     </>
   );
