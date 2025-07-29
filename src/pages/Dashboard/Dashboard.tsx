033accd403acd07f1727bb5664a9d444e650570a
Deivid-Martins <deividrocha2005@gmail.com>
2025-07-29 14:27:15 -0300

    feat: added rapid actions section on dashboard

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -16,8 +16,8 @@ export default function dashboard() {
       <Header />
       <main className="mt-28 bg-slate-100 text-gray-800">
         <LateralMenu username={username} />
-        <section>
-          <div className="flex items-center font-bold font-1 gap-4 justify-center">
+        <section className="py-16 px-78">
+          <div className="flex items-center font-bold font-1 gap-4 ">
             <img className="block max-w-26 h-auto" src={saturn} alt="saturn" />
             <div className="flex flex-col gap-2">
               <h1 className="text-xl font-semibold">Bem vindo {username},</h1>
@@ -28,6 +28,27 @@ export default function dashboard() {
             <InfoBadge label="Professores Ativos" value="0000" variant="blue" />
             <InfoBadge label="Alunos Ativos" value="0000" variant="yellow" />
             <InfoBadge label="Escolas Ativas" value="0000" variant="red" />
+          </div>
+          <h1 className="font-bold font-1 text-2xl mt-16">Ações Rápidas</h1>
+          <div className="flex justify-center font-1 font-bold gap-8 mt-8 text-purplish-blue uppercase">
+            <a
+              href="/register/teacher"
+              className="bg-yellow shadow-xl py-4 px-8 rounded-2xl text-center hover:bg-purplish-blue hover:text-yellow transition duration-200"
+            >
+              Cadastrar Professor
+            </a>
+            <a
+              href="/register/student"
+              className="bg-yellow shadow-xl py-4 px-8 rounded-2xl text-center hover:bg-purplish-blue hover:text-yellow transition duration-200"
+            >
+              Cadastrar Aluno
+            </a>
+            <a
+              href="/register/school"
+              className="bg-yellow shadow-xl py-4 px-8 rounded-2xl text-center hover:bg-purplish-blue hover:text-yellow transition duration-200"
+            >
+              Cadastrar Escola
+            </a>
           </div>
         </section>
       </main>
