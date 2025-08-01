21944c19f1359daf4a89b70cd89051870a5a2bb3
Isaque1600 <isaquebatista25@gmail.com>
2025-08-01 01:53:41 -0300

    feat: adiciona rota e formulario de login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,38 +1,13 @@
+import SignInForm from "@/components/SignInForms/SignInForm";
 import { Footer } from "../../components/Footer/Footer";
 import { Header } from "../../components/Header/Header";
-import { SignUpForm } from "../../components/SignUpForm/SignUpForm";
 
 export default function Form() {
   return (
     <>
       <Header />
       <main className="mt-38 mb-10 grid justify-items-center">
-        <SignUpForm
-          title="Conectar-se"
-          fields={[
-            {
-              name: "email",
-              label: "Email",
-              type: "email",
-              placeholder: "Insira seu email",
-            },
-            {
-              name: "password",
-              label: "Senha",
-              type: "password",
-              placeholder: "Insira sua senha",
-            },
-          ]}
-          buttonText="Entrar"
-          onSubmit={(formData) => {
-            console.log("Form submitted:", formData);
-          }}
-          footerLink={{
-            text: "Não possui conta?",
-            linkText: "Conectar",
-            href: "/cadastro",
-          }}
-        />
+        <SignInForm />
       </main>
       <Footer />
     </>
