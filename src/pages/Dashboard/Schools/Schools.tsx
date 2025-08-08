687e3540505bf63d6604068226cbe2440f861ed6
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-08-08 15:55:36 -0300

    refactor: detaching footer and table

--- src/pages/Dashboard/Schools/Schools.tsx
+++ src/pages/Dashboard/Schools/Schools.tsx
@@ -10,34 +10,38 @@ import saturn from "../../../assets/saturn.svg";
 export function Schools() {
   return (
     <>
-      <Header />
-      <LateralMenu username="Placeholder" />
-      <main className="font-1 bg-slate-200 px-64 pt-32 text-gray-800">
-        <div className="flex items-center gap-4">
-          <img className="max-w-24" src={saturn} alt="Saturn" />
-          <div className="flex flex-col gap-2">
-            <h1 className="text-2xl font-semibold">Bem vindo usuário,</h1>
-            <h1 className="text-5xl font-bold">Escolas</h1>
+      <div className="bg-slate-200">
+        <Header />
+        <LateralMenu username="Placeholder" />
+        <main className="font-1 px-64 pt-32 text-gray-800">
+          <div className="flex items-center gap-4">
+            <img className="max-w-24" src={saturn} alt="Saturn" />
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl font-semibold">Bem vindo usuário,</h1>
+              <h1 className="text-5xl font-bold">Escolas</h1>
+            </div>
           </div>
-        </div>
-        <div className="mt-16 flex items-center justify-between">
-          <input
-            type="text"
-            placeholder="Pesquisar por nome"
-            name="searchInput"
-            id="searchInput"
-            className="text-purplish-blue outline-am0 focus:outline-yellow rounded-lg px-2 py-1 font-bold outline-4 transition duration-300"
-          />
-          <div className="flex items-center gap-8 text-2xl font-bold">
-            <RegisterSchoolModal />
-            <RegisterSchoolUserModal />
+          <div className="mt-16 flex items-center justify-between">
+            <input
+              type="text"
+              placeholder="Pesquisar por nome"
+              name="searchInput"
+              id="searchInput"
+              className="text-purplish-blue outline-am0 focus:outline-yellow rounded-lg px-2 py-1 font-bold outline-4 transition duration-300"
+            />
+            <div className="flex items-center gap-8 text-2xl font-bold">
+              <RegisterSchoolModal />
+              <RegisterSchoolUserModal />
+            </div>
           </div>
+          <div className="mt-12">
+            <SchoolTable />
+          </div>
+        </main>
+        <div className="mt-20">
+          <Footer />
         </div>
-        <div className="mt-12">
-          <SchoolTable />
-        </div>
-      </main>
-      <Footer />
+      </div>
     </>
   );
 }
