48b83caba5572e103d9cbebedcc5477066476a81
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-08-08 15:55:36 -0300

    refactor: detaching footer and table

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -22,76 +22,87 @@ export default function Dashboard() {
 
   return (
     <>
-      <Header />
-      <main className="mt-28 bg-slate-100 text-gray-800">
-        <LateralMenu username={username} />
-        <section className="px-78 py-16">
-          <div className="font-1 flex items-center gap-4 font-bold">
-            <img className="block h-auto max-w-26" src={saturn} alt="saturn" />
-            <div className="flex flex-col gap-2">
-              <h1 className="text-xl font-semibold">Bem vindo {username},</h1>
-              <h1 className="text-4xl">PAINEL DE LOGISTICA</h1>
-            </div>
-          </div>
-          <div className="mt-16 flex justify-center gap-8">
-            <InfoBadge label="Professores Ativos" value="0000" variant="blue" />
-            <InfoBadge label="Alunos Ativos" value="0000" variant="yellow" />
-            <InfoBadge label="Escolas Ativas" value="0000" variant="red" />
-          </div>
-          <h1 className="font-1 mt-16 text-2xl font-bold">Ações Rápidas</h1>
-          <div className="font-1 text-purplish-blue mt-8 flex justify-center gap-8 font-bold uppercase">
-            <RegisterTeacherModal />
-            <RegisterStudentModal />
-            <RegisterSchoolModal />
-            <RegisterSchoolUserModal />
-            <RegisterResponsableModal />
-          </div>
-          <div className="mt-16 flex justify-around rounded-2xl bg-slate-300 py-8 shadow-2xl">
-            <div className="bg-purplish-blue-dark rounded-2xl">
-              <div className="font-1 bg-purplish-blue m-3 rounded-md p-1 text-center text-lg font-bold text-gray-100">
-                <h1>Alunos por Maestria</h1>
-              </div>
-              <img src={graphic} alt="grafico" />
-              <div className="font-1 text-md bg-purplish-blue m-3 flex items-center justify-center gap-2 rounded-md p-1 text-center font-bold text-gray-100 uppercase">
-                <span className="text-white">Não Iniciado</span>
-                <span className="text-yellow">Iniciante</span>
-                <span className="text-green-600">Avançado</span>
-                <span className="text-red-600">Mestre</span>
+      <div className="bg-slate-100">
+        <Header />
+        <main className="mt-28 text-gray-800">
+          <LateralMenu username={username} />
+          <section className="px-78 py-16">
+            <div className="font-1 flex items-center gap-4 font-bold">
+              <img
+                className="block h-auto max-w-26"
+                src={saturn}
+                alt="saturn"
+              />
+              <div className="flex flex-col gap-2">
+                <h1 className="text-xl font-semibold">Bem vindo {username},</h1>
+                <h1 className="text-4xl">PAINEL DE LOGISTICA</h1>
               </div>
             </div>
-            <div className="bg-purplish-blue-dark rounded-2xl">
-              <div className="font-1 bg-purplish-blue m-3 rounded-md p-1 text-center text-lg font-bold text-gray-100">
-                <h1>Alunos por Maestria</h1>
+            <div className="mt-16 flex justify-center gap-8">
+              <InfoBadge
+                label="Professores Ativos"
+                value="0000"
+                variant="blue"
+              />
+              <InfoBadge label="Alunos Ativos" value="0000" variant="yellow" />
+              <InfoBadge label="Escolas Ativas" value="0000" variant="red" />
+            </div>
+            <h1 className="font-1 mt-16 text-2xl font-bold">Ações Rápidas</h1>
+            <div className="font-1 text-purplish-blue mt-8 flex justify-center gap-8 font-bold uppercase">
+              <RegisterTeacherModal />
+              <RegisterStudentModal />
+              <RegisterSchoolModal />
+              <RegisterSchoolUserModal />
+              <RegisterResponsableModal />
+            </div>
+            <div className="mt-16 flex justify-around rounded-2xl bg-slate-300 py-8 shadow-2xl">
+              <div className="bg-purplish-blue-dark rounded-2xl">
+                <div className="font-1 bg-purplish-blue m-3 rounded-md p-1 text-center text-lg font-bold text-gray-100">
+                  <h1>Alunos por Maestria</h1>
+                </div>
+                <img src={graphic} alt="grafico" />
+                <div className="font-1 text-md bg-purplish-blue m-3 flex items-center justify-center gap-2 rounded-md p-1 text-center font-bold text-gray-100 uppercase">
+                  <span className="text-white">Não Iniciado</span>
+                  <span className="text-yellow">Iniciante</span>
+                  <span className="text-green-600">Avançado</span>
+                  <span className="text-red-600">Mestre</span>
+                </div>
               </div>
-              <img src={graphic} alt="grafico" />
-              <div className="font-1 text-md bg-purplish-blue m-3 flex items-center justify-center gap-2 rounded-md p-1 text-center font-bold text-gray-100 uppercase">
-                <span className="text-white">Não Iniciado</span>
-                <span className="text-yellow">Iniciante</span>
-                <span className="text-green-600">Avançado</span>
-                <span className="text-red-600">Mestre</span>
+              <div className="bg-purplish-blue-dark rounded-2xl">
+                <div className="font-1 bg-purplish-blue m-3 rounded-md p-1 text-center text-lg font-bold text-gray-100">
+                  <h1>Alunos por Maestria</h1>
+                </div>
+                <img src={graphic} alt="grafico" />
+                <div className="font-1 text-md bg-purplish-blue m-3 flex items-center justify-center gap-2 rounded-md p-1 text-center font-bold text-gray-100 uppercase">
+                  <span className="text-white">Não Iniciado</span>
+                  <span className="text-yellow">Iniciante</span>
+                  <span className="text-green-600">Avançado</span>
+                  <span className="text-red-600">Mestre</span>
+                </div>
               </div>
             </div>
-          </div>
-          <h1 className="font-1 mt-16 mb-4 text-2xl font-bold">
-            Atividades Recentes
-          </h1>
-          <div className="border-purplish-blue font-1 flex w-max flex-col rounded-3xl border-2 px-8 py-4 pl-12 text-lg font-semibold text-red-900">
-            <ul className="flex list-disc flex-col gap-4">
-              <li>
-                Professora ‘ana lima’ foi cadastrada no sistema. (há 3 min)
-              </li>
-              <li>
-                Aluno ‘joão silva guedes’ foi cadastrada no sistema. (há 8 min)
-              </li>
-              <li>
-                A escola ‘Sebastião Guedes da Silva’ foi cadastrada no sistema.
-                (há 15 min)
-              </li>
-            </ul>
-          </div>
-        </section>
-      </main>
-      <Footer />
+            <h1 className="font-1 mt-16 mb-4 text-2xl font-bold">
+              Atividades Recentes
+            </h1>
+            <div className="border-purplish-blue font-1 flex w-max flex-col rounded-3xl border-2 px-8 py-4 pl-12 text-lg font-semibold text-red-900">
+              <ul className="flex list-disc flex-col gap-4">
+                <li>
+                  Professora ‘ana lima’ foi cadastrada no sistema. (há 3 min)
+                </li>
+                <li>
+                  Aluno ‘joão silva guedes’ foi cadastrada no sistema. (há 8
+                  min)
+                </li>
+                <li>
+                  A escola ‘Sebastião Guedes da Silva’ foi cadastrada no
+                  sistema. (há 15 min)
+                </li>
+              </ul>
+            </div>
+          </section>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
--- src/pages/Dashboard/Teachers/Teachers.tsx
+++ src/pages/Dashboard/Teachers/Teachers.tsx
@@ -8,42 +8,46 @@ import saturn from "../../../assets/saturn.svg";
 export function Teachers() {
   return (
     <>
-      <Header />
-      <LateralMenu username="Placeholder" />
-      <main className="font-1 bg-slate-200 px-64 pt-32 text-gray-800">
-        <div className="flex items-center gap-4">
-          <img className="max-w-24" src={saturn} alt="Saturn" />
-          <div className="flex flex-col gap-2">
-            <h1 className="text-2xl font-semibold">Bem vindo usuário,</h1>
-            <h1 className="text-5xl font-bold">Professores</h1>
+      <div className="bg-slate-200">
+        <Header />
+        <LateralMenu username="Placeholder" />
+        <main className="font-1 px-64 pt-32 text-gray-800">
+          <div className="flex items-center gap-4">
+            <img className="max-w-24" src={saturn} alt="Saturn" />
+            <div className="flex flex-col gap-2">
+              <h1 className="text-2xl font-semibold">Bem vindo usuário,</h1>
+              <h1 className="text-5xl font-bold">Professores</h1>
+            </div>
           </div>
-        </div>
-        <div className="mt-16 flex items-center justify-between">
-          <input
-            type="text"
-            placeholder="Pesquisar por nome"
-            name="searchInput"
-            id="searchInput"
-            className="text-purplish-blue outline-am0 focus:outline-yellow rounded-lg px-2 py-1 font-bold outline-4 transition duration-300"
-          />
-          <div className="flex items-center gap-8 text-2xl font-bold">
-            <a
-              className="bg-yellow text-purplish-blue hover:bg-purplish-blue hover:text-yellow rounded-lg px-6 py-2 shadow transition duration-300"
-              href="/edit/teacher"
-            >
-              Editar
-            </a>
-            <a
-              className="bg-yellow text-purplish-blue hover:bg-purplish-blue hover:text-yellow rounded-lg px-6 py-2 shadow transition duration-300"
-              href="/register/teacher"
-            >
-              Cadastrar
-            </a>
+          <div className="mt-16 flex items-center justify-between">
+            <input
+              type="text"
+              placeholder="Pesquisar por nome"
+              name="searchInput"
+              id="searchInput"
+              className="text-purplish-blue outline-am0 focus:outline-yellow rounded-lg px-2 py-1 font-bold outline-4 transition duration-300"
+            />
+            <div className="flex items-center gap-8 text-2xl font-bold">
+              <a
+                className="bg-yellow text-purplish-blue hover:bg-purplish-blue hover:text-yellow rounded-lg px-6 py-2 shadow transition duration-300"
+                href="/edit/teacher"
+              >
+                Editar
+              </a>
+              <a
+                className="bg-yellow text-purplish-blue hover:bg-purplish-blue hover:text-yellow rounded-lg px-6 py-2 shadow transition duration-300"
+                href="/register/teacher"
+              >
+                Cadastrar
+              </a>
+            </div>
           </div>
+          <TeacherTable />
+        </main>
+        <div className="mt-20">
+          <Footer />
         </div>
-        <TeacherTable />
-      </main>
-      <Footer />
+      </div>
     </>
   );
 }
