8645734b57191193f755336db17a15f4951a629f
Jhuliamq <juhmdq@gmail.com>
2025-08-10 15:01:22 -0300

    fix: ajusta css de input

--- src/components/School/EditForm.tsx
+++ src/components/School/EditForm.tsx
@@ -184,7 +184,7 @@ export default function EditSchoolForm({ id, onSuccess }: EditSchoolFormProps) {
                   field.onChange(value);
                 }}
                 placeholder="(83) 99999-9999"
-                className="flex h-13 w-full rounded-lg bg-amber-50 px-6 py-2 text-base text-gray-800 bg-transparent border border-purplish-blue placeholder:text-gray-500 transition-colors ease-in-out duration-200 hover:border-purplish-blue focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50"
+                className="flex h-13 w-full rounded-lg px-6 py-2 text-base text-gray-800 bg-transparent border border-purplish-blue placeholder:text-gray-500 transition-colors ease-in-out duration-200 hover:border-purplish-blue focus-visible:outline-none focus-visible:ring-2 disabled:cursor-not-allowed disabled:opacity-50"
               />
               </div>
             </Form.Item>
