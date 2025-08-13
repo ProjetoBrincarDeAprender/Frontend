a466fdf7001f1d6da0fe913ebff62ec25cb58e21
Isaque1600 <isaquebatista25@gmail.com>
2025-08-13 14:48:09 -0300

    fix: fix import errors

--- src/pages/Dashboard/Schools/Schools.tsx
+++ src/pages/Dashboard/Schools/Schools.tsx
@@ -1,8 +1,8 @@
 import { RegisterSchoolModal } from "@/components/features/users/school/create/SchoolCreateModal";
 import { RegisterSchoolUserModal } from "@/components/features/users/schoolUser/create/SchoolUserCreateModal";
+import { Footer } from "@/components/Footer/Footer";
+import { Header } from "@/components/Header/Header";
 import { useUser } from "@/hooks/User/useUser";
-import { Footer } from "../../../components/footer/Footer";
-import { Header } from "../../../components/header/Header";
 import { LateralMenu } from "../../../components/sideBar/sideBar";
 
 import SchoolTable from "@/components/features/users/school/files/SchoolTable";
