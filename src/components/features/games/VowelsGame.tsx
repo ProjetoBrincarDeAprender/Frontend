7490eebc33bf975529b36821677782711008c17d
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-08-27 18:54:18 -0300

    feat: game config started

---
+++ src/components/features/games/VowelsGame.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import Phaser from "phaser";
+import Vowels from "@/games/Vowels";
+
+const VowelsGame: React.FC = () => {
+  useEffect(() => {
+    const config: Phaser.Types.Core.GameConfig = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      scene: [Vowels],
+      parent: "game-container",
+    };
+
+    const game = new Phaser.Game(config);
+
+    return () => {
+      game.destroy(true);
+    };
+  }, []);
+
+  return (
+    <>
+      <div id="game-container" className="flex justify-center" />
+    </>
+  );
+};
+
+export default VowelsGame;
