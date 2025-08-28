fdd0580bd5b3c4d62f0d986446a78663e8d75940
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-08-28 17:53:06 -0300

    feat: button changes a image

--- src/games/vowels/Vowels.ts
+++ src/games/vowels/Vowels.ts
@@ -2,15 +2,31 @@ import { EventBus } from "../EventBus";
 import Phaser from "phaser";
 
 export default class Vowels extends Phaser.Scene {
+  private image?: Phaser.GameObjects.Image;
+
+  constructor() {
+    super("Vowels");
+  }
+
   preload() {
     this.load.image("abelha", "/assets/abelha.svg");
+    this.load.image("elefante", "/assets/elefante.svg");
   }
 
   create() {
-    this.add.image(400, 300, "abelha");
+    this.image = this.add.image(400, 300, "abelha");
 
     EventBus.emit("current-scene-ready", "O jogo das vogais foi carregado!");
   }
 
   update() {}
+
+  changeImage() {
+    if (this.image?.texture.key === "abelha") {
+      this.image.setTexture("elefante");
+    } else {
+      this.image?.setTexture("abelha");
+    }
+    console.log("imagem trocada");
+  }
 }
