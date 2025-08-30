8970109df8dd4bafbcab02f42e782a391a853eed
Deivid-Martins <deividrocha2005@gmail.com>
2025-08-30 12:07:21 -0300

    feat: created action button to link teacher and students

--- src/components/features/users/teacher/files/TableData.tsx
+++ src/components/features/users/teacher/files/TableData.tsx
@@ -2,7 +2,7 @@ import type { ColumnDef } from "@tanstack/react-table";
 // import { Edit } from "lucide-react";
 import DeleteModal from "../../../../utils/DataTable/DeleteModal";
 // import { Link } from "../utils/Link/Link";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, Share2 } from "lucide-react";
 import { EditTeacherModal } from "../edit/TeacherEditModal";
 import { Button } from "../../../../ui/button";
 
@@ -68,6 +68,7 @@ export const TeacherColumns: ColumnDef<Teacher>[] = [
     cell: ({ row }) => (
       <div className="flex items-center justify-center gap-2">
         <EditTeacherModal id={row.original.id} />
+        <a className="bg-blue-400 px-3 py-2 shadow-sm rounded-sm transition hover:bg-blue-500" href={`/dashboard/teachers/link-students?id=${row.original.id}`}><Share2 className="text-slate-100" /></a>
         <DeleteModal route="/user/remove" id={row.original.id} />
       </div>
     ),
