4ab25ec0d6ac34996fa41ad4f8298df1346df8ff
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-09-03 18:38:05 -0300

    refactor: change image working

--- src/games/vowels/gameObjects/LetterButton.ts
+++ src/games/vowels/gameObjects/LetterButton.ts
@@ -4,8 +4,7 @@ export class LetterButton extends Phaser.GameObjects.Container {
   private defaultImage: Phaser.GameObjects.Image;
   private hoverImage: Phaser.GameObjects.Image;
   private clickImage: Phaser.GameObjects.Image;
-
-  private texto: Phaser.GameObjects.Text;
+  private letter: Phaser.GameObjects.Text;
 
   constructor(
     scene: Phaser.Scene,
@@ -21,14 +20,14 @@ export class LetterButton extends Phaser.GameObjects.Container {
     this.defaultImage = scene.add.image(0, 0, defaultImage);
     this.hoverImage = scene.add.image(0, 0, hoverImage);
     this.clickImage = scene.add.image(0, 0, clickImage);
-    this.texto = scene.add.text(0, 0, letter).setOrigin(0.5);
+    this.letter = scene.add.text(0, 0, letter).setOrigin(0.5);
 
     this.setSize(this.defaultImage.width, this.defaultImage.height);
 
     this.add(this.defaultImage);
     this.add(this.hoverImage);
     this.add(this.clickImage);
-    this.add(this.texto);
+    this.add(this.letter);
 
     this.hoverImage.setVisible(false);
     this.clickImage.setVisible(false);
@@ -56,5 +55,10 @@ export class LetterButton extends Phaser.GameObjects.Container {
     this.defaultImage.setVisible(false);
     this.hoverImage.setVisible(false);
     this.clickImage.setVisible(true);
+  }
+
+  getLetter() {
+    const stringLetter: string = this.letter.text;
+    return stringLetter;
   }
 }
