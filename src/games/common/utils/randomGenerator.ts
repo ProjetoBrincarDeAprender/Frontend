221ff7eb1a7f11a35c77009ae83bebd5f3274052
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-09-04 15:13:56 -0300

    feat: random letter index

--- src/games/common/utils/randomGenerator.ts
+++ src/games/common/utils/randomGenerator.ts
@@ -1,7 +1,17 @@
 export default class randomGenerator {
-  static randomIndex(arrayLength: number) {
+  static randomIndex(multiplier: number = 1) {
     let index: number;
-    index = Math.floor(Math.random() * arrayLength);
+    index = Math.floor(Math.random() * multiplier);
     return index;
+  }
+
+  static randomCharacter(restriction?: string | string[]) {
+    let character = "";
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const alphabetLength = alphabet.length;
+
+    character = alphabet[this.randomIndex(alphabetLength)];
+
+    return character;
   }
 }
