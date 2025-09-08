0c3716eed586ba7660ea6497233ffa281818812f
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-09-08 18:37:16 -0300

    feat: responsible inital page

--- src/components/studentCard/StudentCard.tsx
+++ src/components/studentCard/StudentCard.tsx
@@ -1,12 +1,13 @@
 import "./studentCard.css";
 import profile from "../../assets/astronauta-profile.svg";
+import { Link } from "react-router";
 
 export default function StudentCard() {
   return (
     // Divisao geral
-    <div className="pt-27">
+    <div>
       {/* Card */}
-      <div className="mx-auto my-20 flex max-w-4xl gap-8 rounded-lg bg-sky-800 p-10">
+      <div className="mx-auto mt-10 mb-20 flex max-w-4xl gap-8 rounded-lg bg-sky-800 p-10 shadow-xl">
         {/* imagem */}
         <div className="flex flex-col justify-items-center gap-4">
           <img
@@ -20,14 +21,16 @@ export default function StudentCard() {
         </div>
 
         {/* Tempo */}
-        <div>
-          <span>00:00</span>
+        <div className="baloobhai border-amarelo flex flex-col items-center justify-center gap-4 rounded-lg border-8 bg-slate-100 px-4 py-8 text-center text-xl font-bold text-stone-700">
+          <span className="rounded-full border-6 border-sky-800 px-4 py-7">
+            00:00
+          </span>
           <span>Tempo hoje</span>
         </div>
 
         {/* Dificuldade */}
-        <div>
-          <h2>Dificuldade</h2>
+        <div className="flex flex-col items-center gap-4 rounded-lg border-8 border-red-900 bg-slate-100 p-4 text-center font-bold text-stone-700">
+          <h2 className="text-2xl text-red-900">Dificuldade</h2>
           <p>
             Notou-se que nos exercícios correlacionaos a experiências
             matemáticas, [user] tem dificuldades notáveis. é necessário
@@ -36,19 +39,40 @@ export default function StudentCard() {
         </div>
 
         {/* Parte da direita*/}
-        <div>
+        <div className="flex flex-col gap-10">
           {/* Estrelinha */}
-          <div>
+          <div className="baloobhai border-amarelo rounded-full border-6 bg-slate-100 px-2 text-end font-bold text-stone-700">
             <img src="" alt="" />
             <span>0/1000</span>
           </div>
 
           {/* Menu de itens */}
-          <div>
-            <span>Voltar</span>
-            <span>Trilha</span>
-            <span>Detalhes</span>
-          </div>
+          <ul className="flex flex-col gap-2">
+            <li>
+              <Link
+                to="/"
+                className="blue bg-yellow hover:bg-purplish-blue hover:text-yellow flex min-h-[60px] justify-center gap-8 rounded-2xl px-8 py-4 text-center font-bold text-stone-700 uppercase shadow-xl transition duration-200"
+              >
+                Voltar
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="blue bg-yellow hover:bg-purplish-blue hover:text-yellow flex min-h-[60px] justify-center gap-8 rounded-2xl px-8 py-4 text-center font-bold text-stone-700 uppercase shadow-xl transition duration-200"
+              >
+                Trilha
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                className="blue bg-yellow hover:bg-purplish-blue hover:text-yellow flex min-h-[60px] justify-center gap-8 rounded-2xl px-8 py-4 text-center font-bold text-stone-700 uppercase shadow-xl transition duration-200"
+              >
+                Detalhes
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
--- src/pages/Dashboard/Responsible/ReponsibleDashboard.tsx
+++ src/pages/Dashboard/Responsible/ReponsibleDashboard.tsx
@@ -1,18 +1,29 @@
 import { Footer } from "@/components/Footer/Footer";
 import { Header } from "@/components/Header/Header";
 import { useUser } from "@/hooks/User/useUser";
-// import { LateralMenu } from "@/components/sideBar/sideBar";
+import { LateralMenu } from "@/components/sideBar/sideBar";
 import StudentCard from "@/components/studentCard/StudentCard";
+import saturn from "../../../assets/saturn.svg";
 
 export function ResponsibleDashboard() {
-  //   const { user } = useUser();
-  //   const username = user?.nome_completo || "Usuário";
+  const { user } = useUser();
+  const username = user?.nome_completo || "Usuário";
 
   return (
-    <>
+    <div className="flex h-fit flex-col bg-neutral-200 pt-28 text-gray-800">
+      <section className="h-full px-78 pt-8">
+        <div className="flex items-center gap-4">
+          <img className="max-w-24" src={saturn} alt="Saturn" />
+          <div className="flex flex-col gap-2">
+            <h1 className="text-2xl font-semibold">Bem vindo {username},</h1>
+            <h1 className="text-5xl font-bold">Alunos sob sua tutela</h1>
+          </div>
+        </div>
+      </section>
+
       <Header />
       <StudentCard />
       <Footer />
-    </>
+    </div>
   );
 }
