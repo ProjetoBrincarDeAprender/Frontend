52ddecb7818781a158aa7c3c69455a95ac0b0958
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-09-27 10:33:33 -0300

    feat: tela de início com background e botões de entrada e saída de jogo

--- src/games/vowels/scenes/StartScene.ts
+++ src/games/vowels/scenes/StartScene.ts
@@ -1,50 +1,97 @@
+import ButtonManager from "@/games/common/managers/ButtonManager";
 import EffectManager from "@/games/common/managers/EffectManager";
 import Phaser from "phaser";
 
 export default class Vowels extends Phaser.Scene {
+  private buttonManager: ButtonManager;
   private effectManager: EffectManager;
 
   constructor() {
     super("vowelsStart");
+    this.buttonManager = new ButtonManager(this);
     this.effectManager = new EffectManager(this);
   }
 
   preload() {
-    this.load.image("startScreen", "/assets/vowelsGame/startScreen.png");
+    this.load.image("background", "/assets/vowelsGame/background.jpeg");
+    this.load.image(
+      "hoverButtonRectangle",
+      "/assets/common/hoverButtonRectangle.svg",
+    );
+    this.load.image(
+      "defaultButtonRectangle",
+      "/assets/common/defaultButtonRectangle.svg",
+    );
+    this.load.image(
+      "clickedButtonRectangle",
+      "/assets/common/clickedButtonRectangle.svg",
+    );
+    this.load.image(
+      "defaultRectangleRed",
+      "/assets/common/defaultRectangleRed.svg",
+    );
+    this.load.image(
+      "hoverRectangleRed",
+      "/assets/common/hoverRectangleRed.svg",
+    );
+    this.load.image(
+      "clickedRectangleRed",
+      "/assets/common/clickedRectangleRed.svg",
+    );
   }
 
   create() {
     const gameWidth = this.cameras.main.width;
     const gameHeight = this.cameras.main.height;
-    this.add.image(gameWidth / 2, gameHeight / 2, "startScreen");
-    this.effectManager.overlay(0.5);
+
+    this.createBackground();
 
     this.add
       .text(gameWidth / 2, gameHeight / 2 - 100, "Jogo das Vogais", {
+        color: "#ffffff",
         fontFamily: "Verdana, Geneva, sans-serif",
         fontSize: "64px",
-        color: "#ffffff",
         fontStyle: "bold",
       })
       .setOrigin(0.5)
       .setDepth(10);
 
-    const startText = this.add
-      .text(gameWidth / 2, gameHeight / 2 + 50, "Clique para começar", {
-        fontFamily: "Verdana, Geneva, sans-serif",
-        fontSize: "22px",
-        color: "#ffffff",
-        fontStyle: "bold",
-      })
-      .setOrigin(0.5)
-      .setDepth(10);
+    const startButton = this.buttonManager.createButton(
+      { x: gameWidth / 2, y: gameHeight / 2 + 60 },
+      [
+        "defaultButtonRectangle",
+        "hoverButtonRectangle",
+        "clickedButtonRectangle",
+      ],
+      "▶ Iniciar",
+      40,
+    );
 
-    this.effectManager.floatingElement(startText);
+    const exitButton = this.buttonManager.createButton(
+      { x: gameWidth / 2, y: gameHeight / 2 + 140 },
+      ["defaultRectangleRed", "hoverRectangleRed", "clickedRectangleRed"],
+      "Sair",
+      40,
+      0.7,
+    );
 
-    this.input.once("pointerdown", () => {
+    startButton.setInteractive().on("pointerup", () => {
       this.scene.start("vowelsGameScene");
+    });
+
+    exitButton.setInteractive().on("pointerup", () => {
+      window.history.back();
     });
   }
 
   update() {}
+
+  private createBackground(): void {
+    const background = this.add.image(400, 300, "background");
+    const scaleX = this.cameras.main.width / background.width;
+    const scaleY = this.cameras.main.height / background.height;
+    const scale = Math.max(scaleX, scaleY);
+    background.setScale(scale);
+    this.effectManager.overlay(0.3);
+  }
 }
