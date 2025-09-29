f98d525ab5f3f255fcfd894f982d9c233de4ecfb
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-09-29 15:42:29 -0300

    fix: erros durante refatoração corrigidos

--- src/games/common/effects/MoveEffect.ts
+++ src/games/common/effects/MoveEffect.ts
@@ -15,10 +15,8 @@ export default function MoveEffect<T extends Phaser.GameObjects.GameObject>(
     targets: targets,
     x: x,
     onComplete: () => {
-      // Quando terminar, reposiciona no lado esquerdo e inicia novamente
       (targets as any).x = -160;
 
-      // Reinicia o tween se repeat é -1 (infinito)
       if (repeat === -1) {
         MoveEffect(scene, targets, x, duration, repeat, 0);
       }
