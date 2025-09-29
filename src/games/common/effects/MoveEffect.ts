0bda866b771df194214b76e27336b1dd134f4112
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-09-29 15:20:28 -0300

    fix: nuvens aparecendo de repente em pontos que não deveria

--- src/games/common/effects/MoveEffect.ts
+++ src/games/common/effects/MoveEffect.ts
@@ -1,18 +1,5 @@
 import Phaser from "phaser";
 
-interface MoveEffectConfig {
-  scene: Phaser.Scene;
-  target: Phaser.GameObjects.GameObject;
-  endX: number;
-  duration: number;
-  repeat: number;
-  delay?: number;
-  resetX?: number;
-}
-
-export default function MoveEffect<T extends Phaser.GameObjects.GameObject>(
-  config: MoveEffectConfig,
-): void;
 export default function MoveEffect<T extends Phaser.GameObjects.GameObject>(
   scene: Phaser.Scene,
   targets: T,
@@ -20,60 +7,20 @@ export default function MoveEffect<T extends Phaser.GameObjects.GameObject>(
   duration: number,
   repeat: number,
   delay: number,
-): void;
-export default function MoveEffect<T extends Phaser.GameObjects.GameObject>(
-  sceneOrConfig: Phaser.Scene | MoveEffectConfig,
-  targets?: T,
-  x?: number,
-  duration?: number,
-  repeat?: number,
-  delay?: number,
 ) {
-  let config: MoveEffectConfig;
-
-  if (typeof sceneOrConfig === "object" && "scene" in sceneOrConfig) {
-    config = sceneOrConfig as MoveEffectConfig;
-  } else {
-    config = {
-      scene: sceneOrConfig as Phaser.Scene,
-      target: targets!,
-      endX: x!,
-      duration: duration!,
-      repeat: repeat!,
-      delay: delay || 0,
-      resetX: -160,
-    };
-  }
-
-  const {
-    scene,
-    target,
-    endX,
-    duration: tweenDuration,
-    repeat: repeatCount,
-    delay: tweenDelay = 0,
-    resetX = -160,
-  } = config;
-
   scene.tweens.add({
-    delay: tweenDelay,
-    duration: tweenDuration,
+    delay: delay,
+    duration: duration,
     repeat: 0,
-    targets: target,
-    x: endX,
+    targets: targets,
+    x: x,
     onComplete: () => {
-      (target as any).x = resetX;
+      // Quando terminar, reposiciona no lado esquerdo e inicia novamente
+      (targets as any).x = -160;
 
-      if (repeatCount === -1) {
-        MoveEffect({
-          scene,
-          target,
-          endX,
-          duration: tweenDuration,
-          repeat: repeatCount,
-          delay: 0,
-          resetX,
-        });
+      // Reinicia o tween se repeat é -1 (infinito)
+      if (repeat === -1) {
+        MoveEffect(scene, targets, x, duration, repeat, 0);
       }
     },
   });
