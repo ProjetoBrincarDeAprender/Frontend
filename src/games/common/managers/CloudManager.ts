2cfb5510065b8c59c0e2848a20de123d3ca0f740
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-09-29 16:14:22 -0300

    refactor: criação do assetLoader e CloudManager para ajudar em manutenções futuras de código

---
+++ src/games/common/managers/CloudManager.ts
@@ -0,0 +1,107 @@
+import RandomGenerator from "../utils/randomGenerator";
+import EffectManager from "./EffectManager";
+import Phaser from "phaser";
+
+interface CloudConfig {
+  textures: string[];
+  count: { min: number; max: number };
+  scale: { min: number; max: number };
+  position: {
+    x: { min: number; max: number };
+    y: { min: number; max: number };
+    spaceBetweenClouds: number;
+  };
+  animation: {
+    speed: { min: number; max: number };
+    delay: { min: number; max: number };
+    endX: number;
+  };
+}
+
+export default class CloudManager {
+  private scene: Phaser.Scene;
+  private effectManager: EffectManager;
+  private readonly cloudConfig: CloudConfig = {
+    textures: ["cloud", "cloud2"],
+    count: { min: 4, max: 6 },
+    scale: { min: 15, max: 55 },
+    position: {
+      x: { min: -160, max: 600 },
+      y: { min: 50, max: 400 },
+      spaceBetweenClouds: 50,
+    },
+    animation: {
+      speed: { min: 2000, max: 4000 },
+      delay: { min: 0, max: 1000 },
+      endX: 920,
+    },
+  };
+
+  constructor(scene: Phaser.Scene) {
+    this.scene = scene;
+    this.effectManager = new EffectManager(scene);
+  }
+
+  private createClouds(): Phaser.GameObjects.Image[] {
+    const clouds: Phaser.GameObjects.Image[] = [];
+    const numberOfClouds = RandomGenerator.randomNumber(
+      this.cloudConfig.count.min,
+      this.cloudConfig.count.max,
+    );
+
+    for (let i = 0; i < numberOfClouds; i++) {
+      const cloud = this.createSingleCloud(i);
+      clouds.push(cloud);
+    }
+
+    return clouds;
+  }
+
+  generateClouds(): void {
+    const clouds = this.createClouds();
+    this.animateClouds(clouds);
+  }
+
+  private createSingleCloud(iteration: number): Phaser.GameObjects.Image {
+    const randomTextureIndex = RandomGenerator.randomNumber(
+      0,
+      this.cloudConfig.textures.length - 1,
+    );
+    const randomTexture = this.cloudConfig.textures[randomTextureIndex];
+    const randomScale =
+      RandomGenerator.randomNumber(
+        this.cloudConfig.scale.min,
+        this.cloudConfig.scale.max,
+      ) / 100;
+    const randomX = RandomGenerator.randomNumber(
+      this.cloudConfig.position.x.min,
+      this.cloudConfig.position.x.max,
+    );
+    const randomY =
+      this.cloudConfig.position.spaceBetweenClouds * (iteration + 1) +
+      RandomGenerator.randomNumber(0, 25);
+
+    return this.scene.add
+      .image(randomX, randomY, randomTexture)
+      .setOrigin(0.5)
+      .setScale(randomScale);
+  }
+
+  animateClouds(clouds: Phaser.GameObjects.Image[]): void {
+    for (let cloud of clouds) {
+      const randomSpeed =
+        RandomGenerator.randomNumber(
+          this.cloudConfig.animation.speed.min,
+          this.cloudConfig.animation.speed.max,
+        ) *
+        (cloud.scale * 10);
+      this.effectManager.move(
+        cloud,
+        this.cloudConfig.animation.endX + cloud.scale * 100,
+        randomSpeed,
+        -1,
+        0,
+      );
+    }
+  }
+}
--- src/games/vowels/scenes/StartScene.ts
+++ src/games/vowels/scenes/StartScene.ts
@@ -2,68 +2,98 @@ import ButtonManager from "@/games/common/managers/ButtonManager";
 import EffectManager from "@/games/common/managers/EffectManager";
 import Phaser from "phaser";
 import RandomGenerator from "@/games/common/utils/randomGenerator";
+import AssetLoader from "@/games/common/loaders/AssetLoader";
+import CloudManager from "@/games/common/managers/CloudManager";
+
+interface CloudConfig {
+  textures: string[];
+  count: { min: number; max: number };
+  scale: { min: number; max: number };
+  position: {
+    x: { min: number; max: number };
+    y: { min: number; max: number };
+  };
+  animation: {
+    speed: { min: number; max: number };
+    delay: { min: number; max: number };
+    endX: number;
+  };
+}
 
 export default class Vowels extends Phaser.Scene {
+  private assetLoader: AssetLoader;
   private buttonManager: ButtonManager;
+  private cloudManager: CloudManager;
   private effectManager: EffectManager;
+
+  private cloudConfig: CloudConfig = {
+    textures: ["cloud", "cloud2"],
+    count: { min: 4, max: 6 },
+    scale: { min: 15, max: 55 },
+    position: {
+      x: { min: -160, max: 600 },
+      y: { min: 50, max: 400 },
+    },
+    animation: {
+      speed: { min: 2000, max: 4000 },
+      delay: { min: 0, max: 1000 },
+      endX: 920,
+    },
+  };
 
   constructor() {
     super("vowelsStart");
     this.buttonManager = new ButtonManager(this);
     this.effectManager = new EffectManager(this);
+    this.assetLoader = new AssetLoader(this);
+    this.cloudManager = new CloudManager(this);
   }
 
   preload() {
-    this.load.image(
-      "backgroundStart",
-      "/assets/vowelsGame/images/backgroundMain.png",
-    );
-    this.load.image(
-      "hoverButtonRectangle",
-      "/assets/common/hoverButtonRectangle.svg",
-    );
-    this.load.image(
-      "defaultButtonRectangle",
-      "/assets/common/defaultButtonRectangle.svg",
-    );
-    this.load.image(
-      "clickedButtonRectangle",
-      "/assets/common/clickedButtonRectangle.svg",
-    );
-    this.load.image(
-      "defaultRectangleRed",
-      "/assets/common/defaultRectangleRed.svg",
-    );
-    this.load.image(
-      "hoverRectangleRed",
-      "/assets/common/hoverRectangleRed.svg",
-    );
-    this.load.image(
-      "clickedRectangleRed",
-      "/assets/common/clickedRectangleRed.svg",
-    );
-    this.load.image("cloud", "/assets/vowelsGame/images/cloud.png");
-    this.load.image("cloud2", "/assets/vowelsGame/images/cloud2.png");
+    this.assetLoader.preloadVowelsStart();
+    this.assetLoader.preloadClouds();
   }
 
   create() {
-    const gameWidth = this.cameras.main.width;
-    const gameHeight = this.cameras.main.height;
-
     this.createBackground();
+    this.createTitle();
+    this.createButtons();
+  }
 
+  update() {}
+
+  private createBackground(): void {
+    const background = this.add.image(400, 300, "backgroundStart");
+    const scaleX = this.cameras.main.width / background.width;
+    const scaleY = this.cameras.main.height / background.height;
+    const scale = Math.max(scaleX, scaleY);
+    background.setScale(scale);
+
+    this.cloudManager.generateClouds();
+
+    this.effectManager.overlay(0.3);
+  }
+
+  private createTitle(): void {
     this.add
-      .text(gameWidth / 2, gameHeight / 2 - 100, "Jogo das Vogais", {
-        color: "#ffffff",
-        fontFamily: "Verdana, Geneva, sans-serif",
-        fontSize: "64px",
-        fontStyle: "bold",
-      })
+      .text(
+        this.cameras.main.width / 2,
+        this.cameras.main.height / 2 - 100,
+        "Jogo das Vogais",
+        {
+          color: "#ffffff",
+          fontFamily: "Verdana, Geneva, sans-serif",
+          fontSize: "64px",
+          fontStyle: "bold",
+        },
+      )
       .setOrigin(0.5)
       .setDepth(10);
+  }
 
+  private createButtons(): void {
     const startButton = this.buttonManager.createButton(
-      { x: gameWidth / 2, y: gameHeight / 2 + 60 },
+      { x: this.cameras.main.width / 2, y: this.cameras.main.height / 2 + 60 },
       [
         "defaultButtonRectangle",
         "hoverButtonRectangle",
@@ -74,7 +104,7 @@ export default class Vowels extends Phaser.Scene {
     );
 
     const exitButton = this.buttonManager.createButton(
-      { x: gameWidth / 2, y: gameHeight / 2 + 140 },
+      { x: this.cameras.main.width / 2, y: this.cameras.main.height / 2 + 140 },
       ["defaultRectangleRed", "hoverRectangleRed", "clickedRectangleRed"],
       "Sair",
       40,
@@ -88,64 +118,5 @@ export default class Vowels extends Phaser.Scene {
     exitButton.setInteractive().on("pointerup", () => {
       window.history.back();
     });
-  }
-
-  update() {}
-
-  private createBackground(): void {
-    const background = this.add.image(400, 300, "backgroundStart");
-    const scaleX = this.cameras.main.width / background.width;
-    const scaleY = this.cameras.main.height / background.height;
-    const scale = Math.max(scaleX, scaleY);
-    background.setScale(scale);
-
-    const cloudTextures = ["cloud", "cloud2"];
-    const clouds = this.createClouds(cloudTextures);
-
-    for (let cloud of clouds) {
-      const randomSpeed =
-        RandomGenerator.randomNumber(2000, 4000) * (cloud.scale * 10);
-      this.effectManager.move(
-        cloud,
-        920 + cloud.scale * 100,
-        randomSpeed,
-        -1,
-        0,
-      );
-    }
-
-    this.effectManager.overlay(0.3);
-  }
-
-  private createClouds(textures: string[]): Phaser.GameObjects.Image[] {
-    const clouds: Phaser.GameObjects.Image[] = [];
-    const numberOfClouds = RandomGenerator.randomNumber(4, 6);
-
-    const minY = 50;
-    const maxY = 400;
-    const spaceBetweenCloudsY = (maxY - minY) / (numberOfClouds + 1);
-
-    for (let i = 0; i < numberOfClouds; i++) {
-      let randomTextureIndex = RandomGenerator.randomNumber(
-        0,
-        textures.length - 1,
-      );
-      let randomTexture = textures[randomTextureIndex];
-      let randomScale = RandomGenerator.randomNumber(15, 55) / 100;
-      let randomX = RandomGenerator.randomNumber(-160, 600);
-      let randomY =
-        spaceBetweenCloudsY * (i + 1) -
-        minY +
-        RandomGenerator.randomNumber(0, 25);
-
-      clouds.push(
-        this.add
-          .image(randomX, randomY, randomTexture)
-          .setOrigin(0.5)
-          .setScale(randomScale),
-      );
-    }
-
-    return clouds;
   }
 }
