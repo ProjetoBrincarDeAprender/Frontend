dc04cb6c2f7984fb6d2cc1a5ebe8652f989ffdb4
Deivid-Martins <deividrocha2005@gmail.com>
2025-11-05 09:59:12 -0300

    feat: created danger zone in stage 1 on maze game

--- src/games/maze/scenes/GameScene.ts
+++ src/games/maze/scenes/GameScene.ts
@@ -38,8 +38,6 @@ export default class MazeGameScene extends Phaser.Scene {
     // Sons
     this.load.audio("correct", "/assets/common/sounds/correct.mp3");
     this.load.audio("incorrect", "/assets/common/sounds/incorrect.mp3");
-    // Som de introdução para fases relacionadas a formas
-    this.load.audio("formsIntro", "/assets/forms/sounds/intro.mp3");
     this.load.image("star-sparkle", "/assets/common/star.svg");
   }
 
@@ -105,14 +103,7 @@ export default class MazeGameScene extends Phaser.Scene {
     }
     if (this.target) this.target.destroy();
 
-    // Toca o som de início da fase somente se o áudio estiver ligado
-    if (!this.sound.mute && this.sound.get("formsIntro") === null) {
-      this.sound.play("formsIntro", {
-        volume: this.registry.has("soundVolume")
-          ? this.registry.get("soundVolume")
-          : 0.7,
-      });
-    }
+    // (Sem som de introdução específico nesta cena)
 
     const level = this.levels[this.currentLevelIndex];
 
