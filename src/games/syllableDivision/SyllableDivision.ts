6254adcc8dd4f11fa21d8c748d9a80850ff42409
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-12-02 15:59:43 -0300

    feat: torna o número de quadrados variável

--- src/games/syllableDivision/SyllableDivision.ts
+++ src/games/syllableDivision/SyllableDivision.ts
@@ -1,60 +1,54 @@
 import Phaser from "phaser";
 
 export default class SyllableDivision extends Phaser.Scene {
-  constructor() {
+  private syllabes: string[];
+
+  constructor(syllabes: string[] = ["A", "B"]) {
     super({ key: "SyllableDivision" });
+    this.syllabes = syllabes;
   }
 
   preload() {}
 
   create() {
     // Elementos visuais
-    const targets = [];
-    targets.push(this.add.rectangle(400, 100, 80, 80, 0x000000, 0.7));
-    targets.push(this.add.rectangle(400, 100, 80, 80, 0x000000, 0.7));
-    targets.push(this.add.rectangle(400, 100, 80, 80, 0x000000, 0.7));
+    const targets: Phaser.GameObjects.Rectangle[] = [];
+    const dropzones: Phaser.GameObjects.Zone[] = [];
+    const sprites: Phaser.GameObjects.Rectangle[] = [];
 
-    const dropzones = [];
-    dropzones.push(
-      this.add.zone(400, 100, 80, 80).setRectangleDropZone(80, 80),
-    );
-    dropzones.push(
-      this.add.zone(400, 100, 80, 80).setRectangleDropZone(80, 80),
-    );
-    dropzones.push(
-      this.add.zone(400, 100, 80, 80).setRectangleDropZone(80, 80),
-    );
-
-    const sprites = [];
-    sprites.push(this.add.rectangle(0, 0, 50, 50, 0x1b9b50ff, 0.7));
-    sprites.push(this.add.rectangle(0, 0, 50, 50, 0x1b9b50ff, 0.7));
-    sprites.push(this.add.rectangle(0, 0, 50, 50, 0x1b9b50ff, 0.7));
+    this.syllabes.forEach(() => {
+      targets.push(this.add.rectangle(400, 100, 80, 80, 0x000000, 0.7));
+      dropzones.push(
+        this.add.zone(400, 100, 80, 80).setRectangleDropZone(80, 80),
+      );
+      sprites.push(this.add.rectangle(0, 0, 50, 50, 0x1b9b50ff, 0.7));
+    });
 
     // Alinha objetos em linha horizontal
     Phaser.Actions.GridAlign(targets, {
-      width: 3,
+      width: this.syllabes.length,
       height: 1,
       cellWidth: 120,
       cellHeight: 100,
-      x: 400 - (3 * 120) / 2 + 20,
+      x: 400 - (this.syllabes.length * 120) / 2 + 20,
       y: 400,
     });
 
     Phaser.Actions.GridAlign(dropzones, {
-      width: 3,
+      width: this.syllabes.length,
       height: 1,
       cellWidth: 120,
       cellHeight: 100,
-      x: 400 - (3 * 120) / 2 + 20,
+      x: 400 - (this.syllabes.length * 120) / 2 + 20,
       y: 400,
     });
 
     Phaser.Actions.GridAlign(sprites, {
-      width: 3,
+      width: this.syllabes.length,
       height: 1,
       cellWidth: 50,
       cellHeight: 50,
-      x: 400 - (3 * 50) / 2,
+      x: 400 - (this.syllabes.length * 50) / 2,
       y: 300,
     });
 
