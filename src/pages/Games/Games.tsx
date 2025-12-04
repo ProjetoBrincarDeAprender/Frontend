21facba03a2412887936a4d42775cdeb5f392bbb
Kayke Nícolas <kaykenicolas007@gmail.com>
2025-12-04 08:35:05 -0300

    hotfix: erros no dashboard e no destaque dos jogos

--- src/pages/Games/Games.tsx
+++ src/pages/Games/Games.tsx
@@ -221,7 +221,7 @@ export function Games() {
                 onClick={() => handleAreaFilter(area.id)}
                 className={` ${area.color} ${
                   selectedArea === area.id
-                    ? "ring-purplish-blue ring-opacity-80 scale-105 transform-gpu shadow-xl ring-6"
+                    ? "outline-4 outline-offset-1 outline-solid outline-purplish-blue ring-opacity-80 scale-105 transform-gpu shadow-xl"
                     : "hover:scale-105 hover:shadow-lg"
                 } font-1 border-purplish-blue border-opacity-60 hover:border-opacity-100 flex min-h-[60px] transform items-center justify-center rounded-2xl border-2 px-6 py-4 text-2xl text-white backdrop-blur-sm transition-all duration-300 ease-in-out hover:cursor-pointer hover:shadow-blue-800/25 active:scale-95 sm:text-base lg:text-lg`}
               >
