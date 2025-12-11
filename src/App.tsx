b37626209c50eee849b9f058e217d6b3c1df6061
Jhuliamq <juhmdq@gmail.com>
2025-12-11 15:01:02 -0300

    fix: update AuthGuard to allow access for Admin role in Teacher dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -87,8 +87,7 @@ function App() {
       {/* Rotas específicas do Professor */}
       <Route
         path="/dashboard/teacher"
-        // element={<AuthGuard requireAuth role={["Professor", "Admin"]} />}
-        element={<AuthGuard requireAuth role={[UserPerfilEnum.TEACHER]} />}
+        element={<AuthGuard requireAuth role={[UserPerfilEnum.TEACHER, UserPerfilEnum.ADMIN]} />}
       >
         <Route index element={<TeacherDashboard />} />
         <Route path="curriculum/knowledge-areas" element={<KnowledgeAreas />} />
