f315de4d737998a6c89fa557737561b7ea13fd2c
Deivid-Martins <deividrocha2005@gmail.com>
2026-02-16 07:51:35 -0300

    fix: fixed error on linear gradient on hero section

--- src/pages/Home/components/Introduction/Introduction.tsx
+++ src/pages/Home/components/Introduction/Introduction.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/utils/Button/Button";
 import girl from "../../../../assets/girlmainpage.svg";
 import aventura from "../../../../assets/homePage/aventura.svg";
+import bg from "../../../../assets/homePage/backgroundMain.png";
 
 import "./Introduction.css";
 import { Joystick } from "lucide-react";
@@ -11,37 +12,47 @@ interface IntroductionProps {
 
 export function Introduction({ className = "" }: IntroductionProps) {
   return (
-    <section
-      className={`${className} font-1 home-introduction relative flex justify-around pt-32`}
-    >
-      <svg className="absolute top-0 left-0 z-10 h-full" viewBox="0 0 900 500">
-        <polygon points="0,0 900,0 0,900" fill="#302579" />
-      </svg>
-      <div className="relative z-20 flex flex-col gap-8 self-center">
-        <h1 className="text-5xl leading-tight font-bold">
-          Transforme o aprendizado em
-          <br />
-          <span className="inline-flex items-center gap-3">
-            uma <img src={aventura} alt="Aventura" className="h-[2em] w-auto" />{" "}
-            divertida!
-          </span>
-        </h1>
-        <p>
-          No Brincar de Aprender, cada fase é uma nova descoberta.
-          <br /> Jogos feitos para ensinar, incluir e evoluir junto com você.
-        </p>
-        <div className="mt-8">
-          <Button className="!shadow-3xl h-16 w-48 items-center self-start rounded-xl border-4 border-transparent text-center text-xl font-bold hover:border-yellow-400 hover:!bg-blue-900">
-            <Joystick className="mr-2 h-16 w-16" />{" "}
-            <span className="block text-4xl font-bold tracking-wider">
-              Jogar
-            </span>
-          </Button>
-        </div>
+    <div className="blend-container">
+      <div className="intro-box">
+        <section
+          className={`${className} font-1 home-introduction relative mx-auto flex max-w-[1400px] items-center justify-between px-12 py-20`}
+        >
+          <div className="relative z-20 flex flex-col gap-8 self-center">
+            <h1 className="text-5xl leading-tight font-bold">
+              Transforme o aprendizado em
+              <br />
+              <span className="inline-flex items-center gap-3">
+                uma{" "}
+                <img src={aventura} alt="Aventura" className="h-[2em] w-auto" />{" "}
+                divertida!
+              </span>
+            </h1>
+            <p>
+              No Brincar de Aprender, cada fase é uma nova descoberta.
+              <br /> Jogos feitos para ensinar, incluir e evoluir junto com
+              você.
+            </p>
+            <div className="mt-8">
+              <Button className="!shadow-3xl h-16 w-48 items-center self-start rounded-xl border-4 border-transparent text-center text-xl font-bold hover:border-yellow-400 hover:!bg-blue-900">
+                <Joystick className="mr-2 h-16 w-16" />{" "}
+                <span className="block text-4xl font-bold tracking-wider">
+                  Jogar
+                </span>
+              </Button>
+            </div>
+          </div>
+          <div className="relative z-20 flex items-end justify-end">
+            <img src={girl} alt="duda" className="block max-h-[31.25rem]" />
+          </div>
+        </section>
       </div>
-      <div className="relative z-20 self-center">
-        <img src={girl} alt="duda" className="block max-h-[31.25rem]" />
+      <div className="relative h-full w-full">
+        <img
+          src={bg}
+          alt="Background"
+          className="block h-full w-full object-cover"
+        />
       </div>
-    </section>
+    </div>
   );
 }
